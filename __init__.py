42dcb386e86f873b0e14954e06b96653d0547e15
DPN-dpn <doopaang@gmail.com>
2026-09-14 10:40:46 +0900

    1.0.1 버전 기록

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 bl_info = {
     "name": "모더 어댑터",
     "author": "DPN",
-    "version": (1, 0, 0),
+    "version": (1, 0, 1),
     "blender": (2, 80, 0),
     "location": "환경설정(Preferences) > 애드온(Add-ons)",
     "description": "xxmi 모딩 중 불편한 점들을 해결하기 위한 자잘한 기능 지원",
